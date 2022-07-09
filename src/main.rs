d3b7a10c066002a3707891cc5de2d2c4063ed3c6
Nithin Muthukumar <kathrikai30201094@gmail.com>
2022-07-09 17:29:39 -0400

    adding basic template

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 use bevy::prelude::*;
+use clansofmesa::{camera::*,assetloader::*};
+
+
+
 
 fn main() {
     App::new()
+        .insert_resource(ClearColor(Color::rgb(0.5,0.5,0.5)))
+        .insert_resource(WindowDescriptor {
+        present_mode: bevy::window::PresentMode::Fifo,
+        ..default()
+        })
+        .add_plugin(CameraPlugin)
+        .add_plugin(AssetLoadPlugin)
         .add_plugins(DefaultPlugins)
-        .add_startup_system(setup)
         .run();
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-    commands.spawn_bundle(SpriteBundle {
-        texture: asset_server.load("icon.png"),
-        ..Default::default()
-    });
-}
