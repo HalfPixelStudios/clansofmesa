0954d825e7dfe6810741f141a8315bc0bc01244f
Daniel Liu <mr.picklepinosaur@gmail.com>
2022-07-13 03:40:05 -0400

    bevy bobs refactor

--- crates/bevy_bobs/src/lib.rs
+++ crates/bevy_bobs/src/lib.rs
@@ -1,5 +1,8 @@
 pub mod component;
 pub mod misc;
+#[cfg(feature = "physics_2d")]
 pub mod physics_2d;
+#[cfg(feature = "prefab")]
 pub mod prefab;
-pub mod system;
+#[cfg(feature = "spawn_wave")]
+pub mod spawn_wave;
