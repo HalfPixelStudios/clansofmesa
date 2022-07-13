8becfe8c5033e17282ab83a342f2750a69c36ca6
Nithin Muthukumar <kathrikai30201094@gmail.com>
2022-07-13 03:10:25 -0400

    input changes

--- src/input.rs
+++ src/input.rs
@@ -5,16 +5,19 @@ use bevy::prelude::*;
 use bytemuck::{Pod, Zeroable};
 use ggrs::PlayerHandle;
 
-pub type PressedPack = i32;
+pub type Pack = i32;
 
-pub const PLACE: PressedPack = 1 << 4;
-pub const CAMERA: PressedPack = 1 << 2;
-pub const ACTION: PressedPack = 1 << 3;
+pub const PLACE: Pack = 1 << 4;
+pub const CAMERA: Pack = 1 << 2;
+pub const ACTION: Pack = 1 << 3;
+pub const INSPECT: Pack = 1 << 5;
 
 #[repr(C)]
 #[derive(Copy, Clone, PartialEq, Pod, Zeroable)]
 pub struct NetInput {
-    pub pressed: PressedPack,
+    pub pressed: Pack,
+    pub just_pressed: Pack,
+
     pub grid_x: i32,
     pub grid_y: i32,
 }
@@ -22,6 +25,7 @@ pub struct NetInput {
 // TODO can define user mapped keys here
 pub fn input_system(
     _: In<PlayerHandle>,
+    btn: Res<Input<MouseButton>>,
     input: Res<Input<KeyCode>>,
     cursor: Res<Cursor>,
     player: Res<LocalPlayerHandle>,
@@ -29,10 +33,14 @@ pub fn input_system(
 ) -> NetInput {
     let mut action = check_action(player, game_data);
 
-    let mut pressed = PressedPack::default();
-
-    if input.pressed(KeyCode::P) && action {
-        pressed |= PLACE;
+    let mut pressed = Pack::default();
+    let mut just_pressed = Pack::default();
+    if btn.just_pressed(MouseButton::Left) {
+        if action {
+            just_pressed |= PLACE;
+        } else {
+            just_pressed |= INSPECT;
+        }
     } else if input.pressed(KeyCode::Q) {
         pressed |= ACTION;
     } else if input.pressed(KeyCode::Escape) {
@@ -42,6 +50,7 @@ pub fn input_system(
 
     NetInput {
         pressed,
+        just_pressed,
         grid_x: grid_x,
         grid_y: grid_y,
     }
