214b65ff16dbb4f05b9b0db9f18fe19444ba1e04
Daniel Liu <mr.picklepinosaur@gmail.com>
2022-07-13 21:31:42 -0400

    player currency system

--- src/player/mod.rs
+++ src/player/mod.rs
@@ -1,1 +1,21 @@
 pub mod place;
+
+use bevy::prelude::*;
+
+#[derive(Component)]
+pub struct Inventory {
+    gems: u32,
+}
+
+impl Inventory {
+    pub fn add_gems(&mut self, amount: u32) {
+        self.gems += amount;
+    }
+    pub fn consume_gems(&mut self, amount: u32) -> Option<u32> {
+        if amount > self.gems {
+            return None;
+        }
+        self.gems -= amount;
+        Some(self.gems)
+    }
+}
